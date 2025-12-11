1f9873a82172a1509f26c373756c41f0b68157a7
7echkilla <prithik1510@gmail.com>
2025-12-12 00:45:35 +1100

    Add motor logic

--- master.cpp
+++ master.cpp
@@ -1,29 +1,48 @@
-#include <Servo.h>
-#define BAUD_RATE 9600
+// // master
 
-Servo steer;
-int xIn, yIn, sel, ang;
+#include <Servo.h>
+
+#define BAUD_RATE 9600
+#define X_PIN A0
+#define Y_PIN A1
+#define SELECT_PIN 2
+#define SERVO_PIN 3
+#define MOTOR_LF 12
+#define MOTOR_LB 13
+#define MOTOR_LS 11
+
+int angle;
 char direction[12];
+Servo steering;
 
 void setup() {
-    pinMode(A0, INPUT);
-    pinMode(A1, INPUT);
-    pinMode(2, INPUT);
-    steer.attach(3);
+    pinMode(X_PIN, INPUT);
+    pinMode(Y_PIN, INPUT);
+    pinMode(SELECT_PIN, INPUT);
+
+    steering.attach(SERVO_PIN);
+
+    pinMode(MOTOR_LF, OUTPUT);
+    pinMode(MOTOR_LB, OUTPUT);
+    pinMode(MOTOR_LS, OUTPUT);
 
     Serial.begin(BAUD_RATE);
 }
 
 void loop() {
-    xIn = analogRead(A0);
-    yIn = analogRead(A1);
-    sel = digitalRead(2);
+    int x_in = analogRead(X_PIN);
+    int y_in = analogRead(Y_PIN);
+    int brake = digitalRead(SELECT_PIN);
 
-    sprintf(direction, "%d,%d,%d\n", xIn, yIn, sel);
+    sprintf(direction, "%d,%d,%d\n", x_in, y_in, brake);
     Serial.print(direction);
     
-    ang = map(xIn, 0, 1023, 0, 180);
-    steer.write(ang);
+    int angle = map(x_in, 0, 1023, 0, 180);
+    steering.write(angle);
 
-    delay(200);
+    digitalWrite(MOTOR_LF, HIGH);
+    digitalWrite(MOTOR_LB, LOW);
+    analogWrite(MOTOR_LS, 10); // 0 -> stop, 255 -> max
+
+    delay(1000);
 }
